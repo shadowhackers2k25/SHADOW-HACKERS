951e06ed4a3eb2923bc41b107868015339590b50
SHADOW HACKERS <shadowhackers2k25@gmail.com>
2025-12-18 23:27:04 +0530

    finally changed some colours

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const recipes = [
     ingredients: ["egg", "rice", "onion"],
     time: "20 minutes",
     cost: "Low",
-    tutorial: ["https://www.youtube.com/watch?v=iv1frVobFeQ"]
+    tutorial: ["https://www.youtube.com/watch?v=iv1frVobFeQ"],
+    imageLink: "https://i.pinimg.com/1200x/18/42/b1/1842b123e9778f6fa135ec3a4b8fad8a.jpg"
 
   },
   {
@@ -13,180 +14,239 @@ const recipes = [
     ingredients: ["egg", "onion"],
     time: "10 minutes",
     cost: "Low",
-    tutorial: ["https://www.youtube.com/watch?v=Lr6SwITcROE"]
-
+    tutorial: ["https://www.youtube.com/watch?v=Lr6SwITcROE"],
+    imageLink:"https://i.pinimg.com/1200x/88/9a/e8/889ae8aeb0d9533a953e7924a9f08651.jpg"
   },
   {
     name: "Simple Veg Rice",
     ingredients: ["rice", "onion"],
     time: "15 minutes",
     cost: "Low",
-    tutorial: ["https://www.youtube.com/watch?v=iFOCkjKD5FA"]
+    tutorial: ["https://www.youtube.com/watch?v=iFOCkjKD5FA"],
+    imageLink:"https://i.pinimg.com/1200x/d1/c6/f8/d1c6f87a8e16e160335020c8bbde4c3c.jpg"
   },
   {
     name: "Boiled Eggs",
     ingredients: ["egg"],
     time: "8 minutes",
     cost: "Very Low",
-    tutorial: ["https://www.youtube.com/watch?v=yhOBCmcATnU"]
+    tutorial: ["https://www.youtube.com/watch?v=yhOBCmcATnU"],
+    imageLink:"https://i.pinimg.com/1200x/32/e5/f7/32e5f7746c1301167314ce9960e6b408.jpg"
   }
   ,
   {
     name: "Chocolate Mug Cake",
-    ingredients: ["Flour ","Sugar ","Cocoa powder ","Milk "],
+    ingredients: ["flour","sugar","cocoa powder","milk"],
     time: "10 minutes",
     cost: "Very Low",
-    tutorial: ["https://www.youtube.com/watch?v=YQm0Kk7H8aI"]
+    tutorial: ["https://www.youtube.com/watch?v=2at7z3ZX_lI"],
+    imageLink:"https://i.pinimg.com/1200x/72/0b/20/720b20d59deb5cdfba3b161227a568a5.jpg"
   },
   {
   name: "Bread Omelette",
-  ingredients: ["Bread", "Egg "],
+  ingredients: ["bread", "egg"],
   time: "10 minutes",
   cost: "Very Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=H2lJ8s9X2pU",
-    "https://www.youtube.com/watch?v=4zQp1k8GxXg"
-  ]
+    "https://www.youtube.com/watch?v=9yrP1WmUQ3s",
+    "https://www.youtube.com/watch?v=rNkHiCMKV9o"
+  ],
+  imageLink:"https://i.pinimg.com/1200x/58/37/7c/58377c14ee160928d0fc13878019abbf.jpg"
 },
 {
   name: "Maggi Noodles",
-  ingredients: ["Maggi"],
+  ingredients: ["maggi"],
   time: "5 minutes",
   cost: "Very Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=6yHnJpZ1z0Q",
-    "https://www.youtube.com/watch?v=G3M8yZPq1pA"
-  ]
+    "https://www.youtube.com/watch?v=Qmz2ZaQRK2M",
+    "https://www.youtube.com/watch?v=i6fcX2J0eXU"
+  ],
+  imageLink:"https://i.pinimg.com/736x/6d/41/05/6d41054c9370811cd5481d264f3836f8.jpg"
 },
 {
   name: "Peanut Butter Sandwich",
-  ingredients: ["Bread ", "Peanut Butter "],
+  ingredients: ["bread", "peanut butter"],
   time: "5 minutes",
   cost: "Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=K4yXkq8FZ0I",
-    "https://www.youtube.com/watch?v=3zWZP4R6yQI"
-  ]
+    "https://www.youtube.com/watch?v=qPbaShU7Dqc",
+    "https://www.youtube.com/watch?v=5JhGR4jo-P0"
+  ],
+  imageLink:"https://i.pinimg.com/736x/ed/80/19/ed80192ee801abdc7b96e1d6778ddaa2.jpg"
 },
 {
   name: "Banana Milkshake",
-  ingredients: ["Banana ", "Milk ", "Sugar "],
+  ingredients: ["banana", "milk", "sugar"],
   time: "5 minutes",
   cost: "Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=H3yQ2v6mX7A",
-    "https://www.youtube.com/watch?v=z9LkJ0W1s5E"
-  ]
+    "https://www.youtube.com/watch?v=Ht-4cM4taNo",
+    "https://www.youtube.com/watch?v=mGH3VQqZwdk"
+  ],
+  imageLink:"https://i.pinimg.com/736x/6c/a1/58/6ca1580081dbda867e648e87caf31767.jpg"
 },
 {
   name: "French Toast",
-  ingredients: ["Bread ", "Egg ", "Milk ", "Sugar "],
+  ingredients: ["bread", "egg", "milk", "sugar"],
   time: "10 minutes",
   cost: "Very Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=4N7XzP6tH0Y",
-    "https://www.youtube.com/watch?v=Y3X7WJ5k8nA"
-  ]
+    "https://www.youtube.com/watch?v=Km7KRbKVu88",
+    "https://www.youtube.com/watch?v=vPrtNzvDS5M"
+  ],
+  imageLink:"https://i.pinimg.com/736x/c3/42/8a/c3428ac84166b43ea10cc9c34de2044f.jpg"
 },
 {
   name: "Poha",
-  ingredients: ["Poha", "Onion"],
+  ingredients: ["poha", "onion"],
   time: "10 minutes",
   cost: "Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=Z1k4p9XGZ8E",
-    "https://www.youtube.com/watch?v=7M8ZL3Kk2ZQ"
-  ]
+    "https://www.youtube.com/watch?v=pNzxeWcbVtU",
+    "https://www.youtube.com/watch?v=A2akRqql_S0"
+  ],
+  imageLink:"https://i.pinimg.com/736x/a8/a4/9d/a8a49de5fc1a71c4db0cc2b20c30ab3d.jpg"
 },
 {
   name: "Fruit Chaat",
-  ingredients: ["Any fruits", "Chat masala"],
+  ingredients: ["any fruits", "chat masala"],
   time: "5 minutes",
   cost: "Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=K3Z8F9W5P2A",
-    "https://www.youtube.com/watch?v=9ZL2H8K4X6E"
-  ]
+    "https://www.youtube.com/watch?v=kM7hIQc6HW0",
+    "https://www.youtube.com/watch?v=QkbDGUvX81c"
+  ],
+  imageLink:"https://i.pinimg.com/1200x/f9/35/c7/f935c730ab9b48168e2d69e460eec713.jpg"
 },
 {
   name: "Rice with Ghee & Salt",
-  ingredients: ["Cooked rice ", "Ghee "],
+  ingredients: ["cooked rice", "ghee"],
   time: "5 minutes",
   cost: "Very Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=8H4Z5KX2L6A",
-    "https://www.youtube.com/watch?v=2L6X8H4Z5KA"
-  ]
+    "https://www.youtube.com/watch?v=dOrTqBsvLaI",
+    "https://www.youtube.com/watch?v=0fsQAbxiNdk"
+  ],
+  imageLink:"https://i.pinimg.com/736x/ad/fd/cf/adfdcf7d1edb8ae989c3b1eba688892c.jpg"
 },
 {
   name: "Tea",
-  ingredients: ["Milk", "Tea leaves", "Sugar"],
+  ingredients: ["milk", "tea leaves", "sugar"],
   time: "7 minutes",
   cost: "Very Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=8QX3K7LZ5pA",
-    "https://www.youtube.com/watch?v=5LZK8QX3p7A"
-  ]
+    "https://www.youtube.com/watch?v=0cf4RUMdcnA",
+    "https://www.youtube.com/watch?v=xSB1bMDF0Hw"
+  ],
+  imageLink:"https://i.pinimg.com/1200x/7c/a6/54/7ca65499d4a7d6ee290c2edd656a46f2.jpg"
 },
 {
   name: "Coffee",
-  ingredients: ["Milk", "Coffee powder", "Sugar"],
+  ingredients: ["milk", "coffee powder", "sugar"],
   time: "5 minutes",
   cost: "Very Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=4LZQK8X3P7A",
-    "https://www.youtube.com/watch?v=Z5K8QX3L7PA"
-  ]
+    "https://www.youtube.com/watch?v=PbKmZwniSF4",
+    "https://www.youtube.com/watch?v=eH7UgvJ8zUY"
+  ],
+  imageLink:"https://i.pinimg.com/1200x/c4/73/7e/c4737e013a673e196416210867f9b1f8.jpg"
 },{
   name: "Vegetable Sandwich",
-  ingredients: ["Bread ", "Onion", "Tomato", "Salt", "Butter"],
+  ingredients: ["bread", "onion", "tomato", "salt", "butter"],
   time: "10 minutes",
   cost: "Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=1F9KZ6W8H5A",
-    "https://www.youtube.com/watch?v=Z5L8K4XH2P9"
-  ]
+    "https://www.youtube.com/watch?v=UVVYkuaJlts",
+    "https://www.youtube.com/watch?v=eD2GEGjrCjI"
+  ],
+  imageLink:"https://i.pinimg.com/1200x/31/4d/26/314d268458abfb0b94e9d643cce81a27.jpg"
 },
 {
   name: "Cheese Toast",
-  ingredients: ["Bread ", "Cheese ", "Butter"],
+  ingredients: ["bread", "cheese", "butter"],
   time: "8 minutes",
   cost: "Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=9H8KX5LZ4A2",
-    "https://www.youtube.com/watch?v=K4ZL9H8X5A2"
-  ]
+    "https://www.youtube.com/watch?v=PcFQnteNwvo&vl=en",
+    "https://www.youtube.com/watch?v=HpPeUcFBvBg"
+  ],
+  imageLink:"https://i.pinimg.com/736x/0d/5e/57/0d5e5795c6982fe728c7665d399bf465.jpg"
+
 },
 {
   name: "Masala Omelette",
-  ingredients: ["Egg ", "Onion ", "Salt", "Oil"],
+  ingredients: ["egg", "onion", "salt", "oil"],
   time: "7 minutes",
   cost: "Very Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=8KZL5H9X4A2",
-    "https://www.youtube.com/watch?v=H5A2KZL8X9"
-  ]
+    "https://www.youtube.com/watch?v=uhjH7tlM1Jw",
+    "https://www.youtube.com/watch?v=419TcV1y0tc"
+  ],
+  imageLink:"https://i.pinimg.com/1200x/32/bf/92/32bf925e32e50b213fb4c4aac93cdf7f.jpg"
 },
 {
   name: "Corn Chaat",
-  ingredients: ["Boiled corn ", "Salt", "Butter "],
+  ingredients: ["boiled corn", "salt", "butter"],
   time: "5 minutes",
   cost: "Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=4ZL9K8XH5A2",
-    "https://www.youtube.com/watch?v=H8X5KZ4L9A2"
-  ]
+    "https://www.youtube.com/watch?v=oO56X3bWx6A",
+    "https://www.youtube.com/watch?v=AMPDjSZJARc"
+  ],
+  imageLink:"https://i.pinimg.com/736x/20/24/ff/2024ff2425cfb376d31fb78fbe50d0df.jpg"
 },
 {
   name: "Aloo Fry",
-  ingredients: ["Potato ", "Salt", "Oil"],
+  ingredients: ["potato", "salt", "oil"],
   time: "15 minutes",
   cost: "Very Low",
   tutorial: [
-    "https://www.youtube.com/watch?v=KZ4L9H8X5A2",
-    "https://www.youtube.com/watch?v=5A2XH8KZL9"
-  ]
+    "https://www.youtube.com/watch?v=UiDDAZcv8bE",
+    "https://www.youtube.com/watch?v=DIWp7xFC9Ek"
+  ],
+  imageLink:"https://i.pinimg.com/1200x/39/ae/8f/39ae8fc99886dfc101780ec351ae362f.jpg"
+},{
+  name: "Chicken Curry",
+  ingredients: ["chicken","onion","tomato","garlic","ginger","spices"],
+  time: "40 minutes",
+  cost: "Medium",
+  tutorial: ["https://www.youtube.com/watch?v=fRPDabc-Oh0"],
+  imageLink: "https://i.pinimg.com/736x/cd/f8/37/cdf83760b6f38e6a2dd27a5b09c7ae57.jpg"
 },
+{
+  name: "Spicy Chicken Fry",
+  ingredients: ["chicken","chili powder","turmeric","garlic","oil"],
+  time: "25 minutes",
+  cost: "Low",
+  tutorial: ["https://www.youtube.com/watch?v=QruC7OLfsss"],
+  imageLink: "https://i.pinimg.com/1200x/cc/1d/3c/cc1d3ce7d2c91ebea0a497b1cec9cfce.jpg"
+},
+{
+  name: "Mutton Kosha",
+  ingredients: ["mutton","onion","ginger","garlic","whole spices"],
+  time: "60 minutes",
+  cost: "High",
+  tutorial: ["https://www.youtube.com/watch?v=wv98EdDkgaI"],
+  imageLink: "https://i.pinimg.com/736x/e9/a5/92/e9a592bc11a55224937245014387e1d0.jpg"
+},
+{
+  name: "Chicken Tikka",
+  ingredients: ["chicken","curd","ginger garlic paste","spices"],
+  time: "30 minutes",
+  cost: "Medium",
+  tutorial: ["https://www.youtube.com/watch?v=vUjXS0seYeo&vl=en"],
+  imageLink: "https://i.pinimg.com/1200x/66/d2/cd/66d2cd1642ef44ff621fdc4d9a14fa09.jpg"
+},
+{
+  name: "Egg Chicken Bhurji",
+  ingredients: ["chicken","egg","onion","green chili","spices"],
+  time: "20 minutes",
+  cost: "Low",
+  tutorial: ["https://www.youtube.com/watch?v=UbhqyYNal1A"],
+  imageLink: "https://i.pinimg.com/736x/2d/02/a9/2d02a9466ab4570eaa1a7b8ea20115b6.jpg"
+}
+
 
 
   
@@ -219,17 +279,33 @@ function findRecipes() {
       div.className = "recipe-card";
 
       let links = recipe.tutorial
-        .map(link => `<a href="${link}" target="_blank">▶ Watch Tutorial</a>`)
+        .map((link, i) => ` <a class="tutorial-link" href="${link}" target="_blank" rel="noopener noreferrer" aria-label="Watch tutorial ${i+1} for ${recipe.name}">▶ Watch Tutorial${recipe.tutorial.length > 1 ? ' ' + (i+1) : ''}</a>`)
         .join("<br>");
 
       div.innerHTML = `
         <h3>${recipe.name}</h3>
-        <p><strong>Time:</strong> ${recipe.time}</p>
-        <p><strong>Cost:</strong> ${recipe.cost}</p>
-        ${links}
+        <div class="recipe-content">
+          <div>
+            <p><strong>Time:</strong> ${recipe.time}</p>
+            <p><strong>Cost:</strong> ${recipe.cost}</p>
+            ${links}
+          </div>
+          <div>
+            <img class="img-placeholder" src="placeholder.jpg" data-src="${recipe.imageLink}"/>
+          </div>
+        </div>
       `;
-
       recipeList.appendChild(div);
+
+      // Load the image after adding to DOM
+      const img = div.querySelector("img.img-placeholder");
+      const realImg = new Image();
+      realImg.src = img.dataset.src;
+
+      realImg.onload = () => {
+        img.src = realImg.src;
+        img.classList.add("loaded");
+      };
     }
   });
 
